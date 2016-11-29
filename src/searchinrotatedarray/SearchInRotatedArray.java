a260e5d70d5bad17af7d8804f463bb30487a9863
Wilson Castiblanco <wcastiblancoq@gmail.com>
2016-11-28 21:50:28 -0500

    Fixing validations

--- src/searchinrotatedarray/SearchInRotatedArray.java
+++ src/searchinrotatedarray/SearchInRotatedArray.java
@@ -16,16 +16,33 @@ public class SearchInRotatedArray {
      */
     public static void main(String[] args) {
         int[] array = new int[]{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
-        int numberToSearch = 3;
-        System.out.println(binarySearch(array, numberToSearch));
+        int[] array2 = new int[]{10, 15, 20, 0, 5};
+        int[] array3 = new int[]{50, 5, 20, 30, 40};
+
+        int numberToSearch = 5;
+        System.out.println(binarySearch(array3, numberToSearch));
     }
 
     private static int binarySearch(int[] array, int n) {
         int low = 0;
         int high = array.length - 1;
         int mid;
-        
-        if(n > array[high]){
+
+        int middle = (low + high) / 2;
+
+        if (n == array[middle]) {
+            return middle;
+        } else if (n == array[high]) {
+            return high;
+        } else if (n == array[low]) {
+            return low;
+        }
+
+        if (n > array[middle] && n < array[high]) {
+            low = (low + high) / 2;
+        } else if (n < array[middle] && n > array[low]) {
+            low = 0;
+        } else if (n < array[middle]) {
             high = (low + high) / 2;
         }
 
@@ -38,7 +55,6 @@ public class SearchInRotatedArray {
             } else {
                 return mid;
             }
-
         }
 
         return -1;
